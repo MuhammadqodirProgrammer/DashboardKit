7e3e60c78d57b7920e8cd10c48e2be40131431b0
MuhammadqodirProgrammer <komilovm831@gmail.com>
2023-01-25 11:39:08 +0500

    prosta commit

--- src/components/Trends2/Task.js
+++ src/components/Trends2/Task.js
@@ -1,32 +1,41 @@
-import "./Task.scss"
+import "./Task.scss";
 
 const dataTrend = [
-    {
-      name: "Resolved",
-      num: 449,
-    },
-    {
-      name: "Received",
-      num: 426,
-    },
-    {
-      name: "Average first response time",
-      num: 43,
-    },
-    {
-      name: " Average response time",
-      num:" 3h 8m",
-    },
-    {
-      name: "Resolution within SLA",
-      num: "94%",
-    },
-  ];
-const Task = ()=>{
-    return (
-        <div className="task-boxs">
-        <div className="task_left-boxs">
+  {
+    name: "Resolved",
+    num: 449,
+  },
+  {
+    name: "Received",
+    num: 426,
+  },
+  {
+    name: "Average first response time",
+    num: 43,
+  },
+  {
+    name: " Average response time",
+    num: " 3h 8m",
+  },
+  {
+    name: "Resolution within SLA",
+    num: "94%",
+  },
+];
+const Task = () => {
+let url ="#"
+  return (
+    <div className="task-boxs">
+      <div className="task_left-boxs">
 
+      <div className="task_left-header">
+    <div >
+    <h3>Unresolved tickets</h3>
+        <p style={{color:"grey"}}>Group:<b>Support</b></p>
+    </div>
+
+        <a href={url} >View details</a>
+      </div>
         {dataTrend.map((el) => (
           <div className="task_left-box">
             <p>{el.name}</p>
@@ -34,8 +43,7 @@ const Task = ()=>{
           </div>
         ))}
       </div>
-      </div>
-
-    )
-}
-export default Task
+    </div>
+  );
+};
+export default Task;
